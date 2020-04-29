be8f59bee6ba8666086c4a49321475e315e04414
Edinson Ningles <edinson.ningles@gmail.com>
2020-04-29 13:04:30 +0200

    Remove text from footer in twenty-twenty template

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -28,12 +28,6 @@
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
 						</p><!-- .footer-copyright -->
 
-						<p class="powered-by-wordpress">
-							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
-								<?php _e( 'Powered by WordPress', 'twentytwenty' ); ?>
-							</a>
-						</p><!-- .powered-by-wordpress -->
-
 					</div><!-- .footer-credits -->
 
 					<a class="to-the-top" href="#site-header">
