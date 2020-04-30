f7afbd7d65e2bc5f610ce908b7860f90328ebb47
Edinson Ningles <edinson.ningles@gmail.com>
2020-04-30 20:27:29 +0200

    Add configuration for production website

--- wp-config.php
+++ wp-config.php
@@ -20,22 +20,43 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'aleman_facil' );
+// ** localhost ** //
+// define( 'DB_NAME', 'aleman_facil' );
+
+// /** MySQL database username */
+// define( 'DB_USER', 'root' );
+
+// /** MySQL database password */
+// define( 'DB_PASSWORD', 'root' );
+
+// /** MySQL hostname */
+// define( 'DB_HOST', 'localhost' );
+
+// /** Database Charset to use in creating database tables. */
+// define( 'DB_CHARSET', 'utf8mb4' );
+
+// /** The Database Collate type. Don't change this if in doubt. */
+// define( 'DB_COLLATE', '' );
+
+// ** production ** //
+define( 'DB_NAME', 'DB4139809' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'U4139809' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'EasyDeutsch20millones$$$' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define('DB_HOST', 'rdbms.strato.de');
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+
+
 
 /**#@+
  * Authentication Unique Keys and Salts.
