54ecf084ce918bb4a5f6a8803c3ac14816efe739
Edinson Ningles <edinson.ningles@gmail.com>
2020-04-30 20:53:22 +0200

    Reset wp config for localhost

--- wp-config.php
+++ wp-config.php
@@ -21,40 +21,40 @@
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 // ** localhost ** //
-// define( 'DB_NAME', 'aleman_facil' );
-
-// /** MySQL database username */
-// define( 'DB_USER', 'root' );
-
-// /** MySQL database password */
-// define( 'DB_PASSWORD', 'root' );
-
-// /** MySQL hostname */
-// define( 'DB_HOST', 'localhost' );
-
-// /** Database Charset to use in creating database tables. */
-// define( 'DB_CHARSET', 'utf8mb4' );
-
-// /** The Database Collate type. Don't change this if in doubt. */
-// define( 'DB_COLLATE', '' );
-
-// ** production ** //
-define( 'DB_NAME', 'DB4139809' );
+define( 'DB_NAME', 'aleman_facil' );
 
 /** MySQL database username */
-define( 'DB_USER', 'U4139809' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'EasyDeutsch20millones$$$' );
+define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
-define('DB_HOST', 'rdbms.strato.de');
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+
+// ** production ** //
+// define( 'DB_NAME', 'DB4139809' );
+
+// /** MySQL database username */
+// define( 'DB_USER', 'U4139809' );
+
+// /** MySQL database password */
+// define( 'DB_PASSWORD', 'EasyDeutsch20millones$$$' );
+
+// /** MySQL hostname */
+// define('DB_HOST', 'rdbms.strato.de');
+
+// /** Database Charset to use in creating database tables. */
+// define( 'DB_CHARSET', 'utf8mb4' );
+
+// /** The Database Collate type. Don't change this if in doubt. */
+// define( 'DB_COLLATE', '' );
 
 
 
