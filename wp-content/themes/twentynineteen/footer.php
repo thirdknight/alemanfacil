d470671fed1043f46aabbefc4c5624231fcc3936
Edinson Ningles <edinson.ningles@gmail.com>
2020-05-05 14:57:42 +0200

    Remove wordpress footer in twentynineteen theme

--- wp-content/themes/twentynineteen/footer.php
+++ wp-content/themes/twentynineteen/footer.php
@@ -22,12 +22,6 @@
 			<?php if ( ! empty( $blog_info ) ) : ?>
 				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
 			<?php endif; ?>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentynineteen' ) ); ?>" class="imprint">
-				<?php
-				/* translators: %s: WordPress. */
-				printf( __( 'Proudly powered by %s.', 'twentynineteen' ), 'WordPress' );
-				?>
-			</a>
 			<?php
 			if ( function_exists( 'the_privacy_policy_link' ) ) {
 				the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
